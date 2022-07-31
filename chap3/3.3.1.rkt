92a1eb2cd25aa2dd8d1f3427f4cfcb539f449ee8
IAmPara0x <confusedstudent13@gmail.com>
2022-07-31 20:42:23 +0530

    implement contains-cycle?

--- chap3/3.3.1.rkt
+++ chap3/3.3.1.rkt
@@ -2,6 +2,8 @@
 
 (require rackunit)
 (require r5rs)
+(require rnrs/mutable-pairs-6)
+(require compatibility/mlist)
 
 (define nil '())
 
@@ -57,5 +59,50 @@
 
   (loop x '()))
 
-(define v (list 'a 'b 'c 'd))
-(define w (mystery v))
+;; check if a lists contains a cycle.
+(define (contains-cycle? x)
+
+  (define (check x y n)
+    (cond ((= n 0) (eq? x y))
+          ((eq? x y) #t)
+          (else (check (cdr x) y (- n 1)))
+          ))
+
+  (define (go y n)
+    (cond ((null? y) #f)
+          ((check x (cdr y) n) #t)
+          (else (go (cdr y) (+ n 1)))
+          ))
+
+  (go x 0))
+
+;; Tests
+
+(define non-cyclic-list (mlist 'a 'b 'c 'd 'e 'f 'g))
+(define cyclic-list (make-cycle (mlist 'a 'b 'c 'd 'e 'f 'g 'h)))
+
+(define M (mcons 'u 'b))
+(define N (mcons 'v 'c))
+(define O (mcons 'w 'd))
+(define P (mcons 'd 'e))
+(define Q (mcons 'x 'f))
+(define R (mcons 'y 'g))
+(define S (mcons 'z 'h))
+
+(set-mcdr! M N)
+(set-mcdr! N O)
+(set-mcdr! O P)
+(set-mcdr! P M)
+(set-mcar! P Q)
+(set-mcdr! Q R)
+(set-mcdr! R S)
+(set-mcdr! S P)
+
+(check-true  (contains-cycle? cyclic-list))
+(check-false (contains-cycle? non-cyclic-list))
+
+(set-mcdr! S '())
+(check-true (contains-cycle? M))
+
+(set-mcdr! P Q)
+(check-false (contains-cycle? M))
